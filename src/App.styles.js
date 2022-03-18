cc4c8ab56578288eb9d6ae2e3c1faf37c52184dd
Manthan Kuber <manthankuber873@gmail.com>
2022-03-18 19:33:05 +0530

    Initial Commit

---
+++ src/App.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  margin: 5rem;
+
+  h1 {
+    font: 500 2.4rem "Poppins", sans-serif;
+    color: #4f4f4f;
+  }
+
+  
+`;
